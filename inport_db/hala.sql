afed48e822b2fb562ec7568140c758fe3b97a0d5
rafana <33988405+rafana73@users.noreply.github.com>
2020-12-30 19:23:24 +0100

    Add files via upload

--- inport_db/hala.sql
+++ inport_db/hala.sql
@@ -1,1 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Czas generowania: 30 Gru 2020, 17:44
+-- Wersja serwera: 5.7.32
+-- Wersja PHP: 7.4.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `hala`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `nick` text COLLATE utf8_polish_ci NOT NULL,
+  `text` text COLLATE utf8_polish_ci NOT NULL,
+  `dateTime` datetime NOT NULL,
+  `ip` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `naglowek`
+--
+
+CREATE TABLE `naglowek` (
+  `naglowek_id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `wiadomosc` varchar(220) COLLATE utf8_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` text NOT NULL,
+  `user_pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_pass`) VALUES
+(1, 'admin', '$2y$10$eVuwBiwR4.i8fgMiEhbOue8c8A7V6uaJye2wdTbkgkk3wth/1LX52');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zapisy`
+--
+
+CREATE TABLE `zapisy` (
+  `id` int(11) NOT NULL,
+  `nick` text COLLATE utf8_polish_ci NOT NULL,
+  `team` int(1) NOT NULL,
+  `dateTime` datetime NOT NULL,
+  `ip` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `naglowek`
+--
+ALTER TABLE `naglowek`
+  ADD PRIMARY KEY (`naglowek_id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeksy dla tabeli `zapisy`
+--
+ALTER TABLE `zapisy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `naglowek`
+--
+ALTER TABLE `naglowek`
+  MODIFY `naglowek_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `zapisy`
+--
+ALTER TABLE `zapisy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
